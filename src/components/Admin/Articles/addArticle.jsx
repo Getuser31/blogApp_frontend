1f924f73b595b9b1810754845b7c3a2237123ccb
Charles <charles.fournet@pm.me>
2026-02-12 11:54:55 +0100

    Ensure consistent toolbar order in ReactQuill across article editing and creation components

--- src/components/Admin/Articles/addArticle.jsx
+++ src/components/Admin/Articles/addArticle.jsx
@@ -15,10 +15,10 @@ const modules = {
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
         [{ 'font': [] }],
         [{ 'size': ['small', false, 'large', 'huge'] }],
+        [{ 'color': [] }, { 'background': [] }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
         [{ 'script': 'sub'}, { 'script': 'super' }],
         [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-        [{ 'color': [] }, { 'background': [] }],
         [{ 'align': [] }],
         ['link'],
         ['clean']
